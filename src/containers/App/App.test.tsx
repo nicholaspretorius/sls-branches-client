5167d453a4f4a1ad0a757fc714eac4b5a3753c0b
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-23 16:42:49 +0200

    tests: Change to it for consistency

--- src/containers/App/App.test.tsx
+++ src/containers/App/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-  test("renders learn react link", () => {
+  it("renders learn react link", () => {
     const { getByText } = render(<App />);
     const linkElement = getByText("Hello world!");
     expect(linkElement).toBeInTheDocument();
