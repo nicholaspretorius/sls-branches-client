33e42614c6b4fa469eaa78ac4292020815198662
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-29 15:19:53 +0200

    feat: Add Dashboard outline, add Login

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NotFound from "./containers/NotFound/NotFound";
+import Login from "./containers/Login/Login";
 
 export default function Routes() {
   return (
@@ -9,7 +10,12 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
-      <NotFound />
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   );
 }
---
+++ src/containers/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <h3>Dashboard</h3>
+    </div>
+  );
+}
