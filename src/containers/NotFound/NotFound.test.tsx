5f31a02f1070c5850f5781fb84ea0df3f81d97cc
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-29 15:20:25 +0200

    tests: Add basic tests

--- src/containers/NotFound/NotFound.test.tsx
+++ src/containers/NotFound/NotFound.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import renderWithRouter from "../../setupTests";
 import NotFound from "./NotFound";
 
-describe("Header", () => {
-  it("renders the Navbar", () => {
+describe("NotFound", () => {
+  it("renders the NotFound page", () => {
     const { getByText } = renderWithRouter(<NotFound />);
     const notFound = getByText("Sorry, page not found!");
     expect(notFound.textContent).toBe("Sorry, page not found!");
