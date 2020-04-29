708d78ba7c82d17059d2cdaf5103355ba72c0458
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-29 13:12:40 +0200

    feat: Add Routes, add Home, revise App setup

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import "./App.css";
-
 import Header from "../../components/Header/Header";
+import Routes from "../../Routes";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App container">
       <Header title="Branchly" />
-      <header className="App-header">
-        <p>Hello world!</p>
-      </header>
+      <Routes />
     </div>
   );
 };
