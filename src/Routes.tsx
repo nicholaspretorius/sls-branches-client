c0dca8699e6c36740aaf557370d14459647a17da
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-30 11:27:26 +0200

    feat: Add NewEntity route, update login redirect

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,8 @@ import Home from "./containers/Home/Home";
 import NotFound from "./containers/NotFound/NotFound";
 import Login from "./containers/Login/Login";
 import Register from "./containers/Register/Register";
+import NewEntity from "./containers/NewEntity/NewEntity";
+import Dashboard from "./containers/Dashboard/Dashboard";
 
 export default function Routes() {
   return (
@@ -16,6 +18,12 @@ export default function Routes() {
       </Route>
       <Route exact path="/register">
         <Register />
+      </Route>
+      <Route exact path="/dashboard">
+        <Dashboard />
+      </Route>
+      <Route exact path="/entities/new">
+        <NewEntity />
       </Route>
       <Route>
         <NotFound />
